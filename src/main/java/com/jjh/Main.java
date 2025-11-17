85d569fd3b24899d613a39eba5c492d5eb8191f0
junheackkp-jjh <junheackp@gmail.com>
2025-11-17 16:46:19 +0900

    step1: 게시물 상세보기

--- src/main/java/com/jjh/Main.java
+++ src/main/java/com/jjh/Main.java
@@ -7,6 +7,8 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    Article lastArticle = null;
+
     int lastId = 0;
 
     System.out.println("== 자바 게시판 시작 ==");
@@ -33,7 +35,23 @@ public class Main {
 
         System.out.println("생성 된 게시물 객체 : " + article);
 
+        lastArticle = article;
+
         System.out.printf("%d번 게시물이 등록 되었습니다.\n", id);
+      }
+      else if (cmd.equals("/usr/article/detail")) {
+        System.out.println("== 게시물 상세보기 ==");
+        Article article = lastArticle;
+
+        if (article == null) {
+          System.out.println("해당 게시물은 존재하지 않습니다.");
+          continue;
+        }
+
+        System.out.printf("%d번 게시물 상세보기\n", article.id);
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.title);
+        System.out.printf("내용 : %s\n", article.content);
       }
       else if (cmd.equals("exit")) {
         System.out.println("프로그램을 종료합니다.");
